ad529cece7053329eb1ae506f917afb60a126c6f
DataVizHonduran <alberto.boquin@gmail.com>
2026-01-15 12:51:43 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
             transition={{ delay: 0.3 }}
           >
             PS11 Chess Team Performance Ecosystem
-            (only displays trainers with PLW > 20)
+            -only displays trainers with PLW over 20-
           </motion.p>
         </div>
         <motion.div
